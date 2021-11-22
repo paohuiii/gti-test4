e1a5a02d3058e540540231544ac8c38cc97fba99
huangjinfeng <1611018255@qq.com>
2021-11-22 21:52:59 +0800

    second commit

--- src/main/java/com/smxy/git/gitTest.java
+++ src/main/java/com/smxy/git/gitTest.java
@@ -3,5 +3,6 @@ package com.smxy.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
